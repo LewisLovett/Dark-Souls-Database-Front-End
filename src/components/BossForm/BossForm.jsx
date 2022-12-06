43839bd1aa98a4b84c52a583a8805625e457b33f
Lewis Lovett <llovett7575@gmail.com>
2022-12-06 12:36:36 +0000

    Created BossForm for creating bosses

---
+++ src/components/BossForm/BossForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./BossForm.scss";
+
+const BossForm = ({handleSubmit}) => {
+    const [boss,setBoss] = useState();
+
+    return(
+        <div className="form">
+            <h3>Form</h3>
+            <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Boss name"
+              onInput={event=>setBoss({...boss,bossName:event.target.value})}  />
+            <input type="text" placeholder="Boss health"
+              onInput={event=>setBoss({...boss,bossHealth:event.target.value})}  />
+            <input type="text" placeholder="Boss souls"
+              onInput={event=>setBoss({...boss,bossSouls:event.target.value})}  />
+            <input type="text" placeholder="Boss Image"
+              onInput={event=>setBoss({...boss,bossImage:event.target.value})}  />
+            <input type="text" placeholder="Boss description"
+              onInput={event=>setBoss({...boss,bossDesc:event.target.value})}  />
+              <button type="submit">Submit</button>
+            </form>
+            
+        </div>
+    )
+}
+export default BossForm;
