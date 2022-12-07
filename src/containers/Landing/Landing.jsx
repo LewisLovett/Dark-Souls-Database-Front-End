056488c0a99a6adf3cb797287026e775cdfd597a
Lewis Lovett <llovett7575@gmail.com>
2022-12-07 12:21:28 +0000

    Added styling to landing page

--- src/containers/Landing/Landing.jsx
+++ src/containers/Landing/Landing.jsx
@@ -1,11 +1,11 @@
-
+import "./Landing.scss";
 
 const Home = () => {
     return (
-    <>
-   
-    <h1>DARK SOULS BOSSES DATABASE</h1>
-    </>
+    <div className="landing-container">
+      <h1 className="landing-container__title">DARK SOULS BOSSES DATABASE</h1>
+      <img className="landing-container__logo" alt="Sun Logo" src={require("../../assets/images/sunlight_logo.png")}/>
+    </div>
     )
     
   };
